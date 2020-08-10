0d4398a54f760d14ca05b1f3d1b28f523baaf1e5
techthumb1 <robinsonjason761@gmail.com>
2020-08-09 22:46:39 -0500

    Rendering HTML and JSON responses

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -2,11 +2,20 @@ from flask import Blueprint
 
 home_routes = Blueprint("home_routes", __name__)
 
+
 @home_routes.route("/")
 def index():
-    x = 2 + 2
+    print("VISITING THE HOME PAGE")
+    x = 100 / 20 
     return f"Hello World! {x}"
 
 @home_routes.route("/about")
 def about():
+    print("VISITING THE ABOUT PAGE")
     return "About me"
+
+@home_routes.route("/bio")
+def bio():
+    print("THIS IS ME...")
+    return "Biography"
+
