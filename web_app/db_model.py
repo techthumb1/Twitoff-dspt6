f599e8c9a5ef902716b5b0d045556f07177f0d11
techthumb1 <robinsonjason761@gmail.com>
2020-08-11 22:23:58 -0500

    Adding Basilica and Tweepy API's

--- web_app/db_model.py
+++ web_app/db_model.py
@@ -2,39 +2,30 @@
 # web_app/db_model.py
 
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
-db = SQLAlchemy()
 
-migrate = Migrate()
-
-class Book(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128))
-    author_id = db.Column(db.String(128))
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    follower_count = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
-        return f"<Book {self.id} {self.title}>"
+        return "<User %r>" % self.username
     
-def parse_records(database_records):
-    """
-    A helper method for converting a list of database record objects into a list of dictionaries, so they can be returned as JSON
 
-    Param: database_records (a list of db.Model instances)
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Unicode(300))
+    user = db.relationship('User', backref=db.backref('text', lazy=True))
 
-    Example: parse_records(User.query.all())
+    def __repr__(self):
+        return "<Tweet %r>" % self.text  
 
-    Returns: a list of dictionaries, each corresponding to a record, like...
-        [
-            {"id": 1, "title": "Book 1"},
-            {"id": 2, "title": "Book 2"},
-            {"id": 3, "title": "Book 3"},
-        ]
-    """
-    parsed_records = []
-    for record in database_records:
-        print(record)
-        parsed_record = record.__dict__
-        del parsed_record["_sa_instance_state"]
-        parsed_records.append(parsed_record)
-    return parsed_records
+#def parse_records(database_records):
+#    parsed_records = []
+#    for record in database_records:
+#        print(record)
+#        parsed_record = record.__dict__
+#        del parsed_record["_sa_instance_state"]
+#        parsed_records.append(parsed_record)
+#    return parsed_records
