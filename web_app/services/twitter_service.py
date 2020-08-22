00ef4ab408c2a47b0ffb2b8c988ad2c1dd0400ea
techthumb1 <robinsonjason761@gmail.com>
2020-08-22 00:06:18 -0500

    ___

--- web_app/services/twitter_service.py
+++ web_app/services/twitter_service.py
@@ -9,10 +9,13 @@ TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
 TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
 TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
+BASILICA_API_KEY = os.getenv("BASILICA_API_KEY")
+
 auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
 auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
+#api.update_status('tweepy + oauth!')
 
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
